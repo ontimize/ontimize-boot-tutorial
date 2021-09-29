4d9d9ae5fd6ae1de2cbcecbc62344545d717b65e
Enrique Alvarez Pereira <enrique.alvarez@imatia.com>
2021-09-29 12:31:57 +0200

    Exercise 8: Adding new services

---
+++ hr-ws/src/main/java/com/ontimize/hr/ws/core/rest/OfferRestController.java
@@ -0,0 +1,23 @@
+package com.ontimize.hr.ws.core.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ontimize.hr.api.core.service.IOfferService;
+import com.ontimize.jee.server.rest.ORestController;
+
+@RestController
+@RequestMapping("/offers")
+@ComponentScan(basePackageClasses = { com.ontimize.hr.api.core.service.IOfferService.class })
+public class OfferRestController extends ORestController<IOfferService> {
+
+ @Autowired
+ private IOfferService offerService;
+
+ @Override
+ public IOfferService getService() {
+  return this.offerService;
+ }
+}
