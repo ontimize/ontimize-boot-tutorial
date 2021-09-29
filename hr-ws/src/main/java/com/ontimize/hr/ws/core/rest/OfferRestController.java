048f157a529748d5da58ca2a195cde0fa10d98e3
Enrique Alvarez Pereira <enrique.alvarez@imatia.com>
2021-09-29 14:24:55 +0200

    Exercise 9: Use of Basic Expression and new methods

--- hr-ws/src/main/java/com/ontimize/hr/ws/core/rest/OfferRestController.java
+++ hr-ws/src/main/java/com/ontimize/hr/ws/core/rest/OfferRestController.java
@@ -1,11 +1,27 @@
 package com.ontimize.hr.ws.core.rest;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ontimize.hr.api.core.service.IOfferService;
+import com.ontimize.hr.model.core.dao.OfferDao;
+import com.ontimize.jee.common.db.SQLStatementBuilder;
+import com.ontimize.jee.common.db.SQLStatementBuilder.BasicExpression;
+import com.ontimize.jee.common.db.SQLStatementBuilder.BasicField;
+import com.ontimize.jee.common.db.SQLStatementBuilder.BasicOperator;
+import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.dto.EntityResultMapImpl;
 import com.ontimize.jee.server.rest.ORestController;
 
 @RestController
@@ -20,4 +36,61 @@ public class OfferRestController extends ORestController<IOfferService> {
  public IOfferService getService() {
   return this.offerService;
  }
+
+ @RequestMapping(value = "currentOffers/search", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+ public EntityResult currentOffersSearch(@RequestBody Map<String, Object> req) {
+  try {
+   List<String> columns = (List<String>) req.get("columns");
+   Map<String, Object> key = new HashMap<String, Object>();
+   key.put(SQLStatementBuilder.ExtendedSQLConditionValuesProcessor.EXPRESSION_KEY,
+     searchBetween(OfferDao.ATTR_START_DATE));
+   return offerService.offerQuery(key, columns);
+  } catch (Exception e) {
+   e.printStackTrace();
+   EntityResult res = new EntityResultMapImpl();
+   res.setCode(EntityResult.OPERATION_WRONG);
+   return res;
+  }
+ }
+
+ @RequestMapping(value = "yearOffers/search", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+ public EntityResult yearOffersSearch(@RequestBody Map<String, Object> req) {
+  try {
+   List<String> columns = (List<String>) req.get("columns");
+   Map<String, Object> filter = (Map<String, Object>) req.get("filter");
+   int year = (int) filter.get("YEAR");
+   Map<String, Object> key = new HashMap<String, Object>();
+   key.put(SQLStatementBuilder.ExtendedSQLConditionValuesProcessor.EXPRESSION_KEY,
+     searchBetweenWithYear(OfferDao.ATTR_START_DATE, year));
+   return offerService.offerQuery(key, columns);
+  } catch (Exception e) {
+   e.printStackTrace();
+   EntityResult res = new EntityResultMapImpl();
+   res.setCode(EntityResult.OPERATION_WRONG);
+   return res;
+  }
+ }
+
+ private BasicExpression searchBetween(String param) {
+
+  Calendar cal = Calendar.getInstance();
+  int currentYear = cal.get(Calendar.YEAR);
+
+  return this.searchBetweenWithYear(param, currentYear);
+ }
+
+ private BasicExpression searchBetweenWithYear(String param, int year) {
+
+  Calendar cal = Calendar.getInstance();
+  cal.set(year, 0, 1);
+  Date startDate = cal.getTime();
+  cal.set(year + 1, 0, 1);
+  Date endDate = cal.getTime();
+
+  BasicField field = new BasicField(param);
+  BasicExpression bexp1 = new BasicExpression(field, BasicOperator.MORE_EQUAL_OP, startDate);
+  BasicExpression bexp2 = new BasicExpression(field, BasicOperator.LESS_OP, endDate);
+  return new BasicExpression(bexp1, BasicOperator.AND_OP, bexp2);
+ }
+
 }
